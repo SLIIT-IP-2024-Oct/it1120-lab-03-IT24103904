715c8ae34b2ef573a409a610a980b4b085c72970
IT24103904 <it24103904@my.sliit.lk>
2024-11-01 12:45:51 +0530

    Q2

---
+++ Q2/IT24103904Lab3Q3.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class IT24103904Lab3Q3 {
+
+              public static void main (String[] args){
+
+                   // Declare the variables
+
+                    double monthlySalary, otHours, otHourlyRate, otAmount , totalSalary;
+
+                   // Create a Scanner object to read input
+
+                    Scanner input = new Scanner (System. in);
+
+
+
+                   // Prompt the user to enter the monthly salary
+                     System.out.print("Enter the monthly salary: ");
+
+                      monthlySalary = input.nextDouble();
+
+                    // Prompt the user to enter the number of OT hours
+                    System.out.print("Enter the number of OT hours: ");
+
+                    otHours = input.nextDouble();
+
+                     // Prompt the user to enter the OT hourly rate
+                     System.out.print("Enter your OT hourly rate: ");
+
+                     otHourlyRate = input.nextDouble();
+
+          
+                    //Calculate the OT amount
+                    otAmount = otHours * otHourlyRate;
+
+                    //Calculate the total salary
+                    totalSalary = monthlySalary + otAmount;
+
+
+              //Display the total salary
+              System.out.println();
+              System.out.println("Your Total Salary is : " +totalSalary);
+           }
+}
+
+                     
